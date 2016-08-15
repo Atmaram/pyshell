7f5cd2a43f0ee98548cd5fb88c767b60e18775af
Atmaram Shetye <atmaram.shetye@gmail.com>
2016-08-15 13:11:18 +0530

    Yak shaving for creating a pypi package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "pyshell",
+    packages = "pyshell",
+    version = "0.1",
+    description = "An interactive customizeable shell",
+    author = "Atmaram Shetye",
+    author_email = "atmaram.shetye@gmail.com",
+    url = "https://github.com/Atmaram/pyshell",
+    download_url = "https://github.com/Atmaram/pyshell/tarball/pyshell-0.2",
+    keywords = ['terminal', 'console', 'shell', 'interactive'],
+    classifiers = [],
+)
