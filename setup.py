8044ff3c7f56636416d5ad3f5a127415c98a6368
Atmaram Shetye <atmaram.shetye@gmail.com>
2016-08-15 14:21:56 +0530

    Updated tag

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name = "pyshell",
     packages = ["pyshell", "tests"],
-    version = "0.3",
+    version = "0.4",
     description = "An interactive customizeable shell",
     license = 'MIT',
     author = "Atmaram Shetye",
     author_email = "atmaram.shetye@gmail.com",
     url = "https://github.com/Atmaram/pyshell",
-    download_url = "https://github.com/Atmaram/pyshell/tarball/0.3",
+    download_url = "https://github.com/Atmaram/pyshell/tarball/0.4",
     keywords = ['terminal', 'console', 'shell', 'interactive'],
     classifiers = [],
 )
